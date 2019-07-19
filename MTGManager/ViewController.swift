9d7bc2f1950c58f796e74cb51b559d899fbba025
Noah Saldana <saldanan@wustl.edu>
2019-07-19 10:02:25 -0400

    Initial Commit

---
+++ MTGManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MTGManager
+//
+//  Created by Noah Saldaña on 7/19/19.
+//  Copyright © 2019 Noah Saldaña. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
